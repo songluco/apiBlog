7c8bbc9ccc78851162f8dc740369678fa631df0a
宋路 <song.l@haihangyun.com>
2018-03-10 19:58:53 +0800

    我也尝试着使用一下laravel邮件系统

---
+++ resources/views/emails/user.blade.php
@@ -0,0 +1,15 @@
+@component('mail::message')
+# SongLu blog
+
+Chinese authorities respond to allegations of China meddling in foreign affairs through its 'sharp power.'
+Chinese authorities respond to allegations of China meddling in foreign affairs through its 'sharp power.'
+Chinese authorities respond to allegations of China meddling in foreign affairs through its 'sharp power.'
+Chinese authorities respond to allegations of China meddling in foreign affairs through its 'sharp power.'
+
+@component('mail::button', ['url' => ''])
+验证用户
+@endcomponent
+
+Thanks,<br>
+{{ config('app.name') }}
+@endcomponent
